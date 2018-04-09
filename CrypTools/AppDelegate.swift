8ae89138eefbdae531546bd61983dd3a1cd5aa3f
Arthur Guiot <arguiot@gmail.com>
2018-04-09 09:42:19 +0800

    Initial Commit

---
+++ CrypTools/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CrypTools
+//
+//  Created by Arthur Guiot on 09/04/2018.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CrypTools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CrypTools
+//
+//  Created by Arthur Guiot on 09/04/2018.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
